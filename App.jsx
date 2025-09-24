e3afd424b4c08905d490ee816c3820821d03e927
sivaranjini1510 <2315048@nec.edu.in>
2025-09-24 20:54:12 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PatientForm from './components/PatientForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PatientForm />
+    </div>
+  );
+}
+
+export default App;
